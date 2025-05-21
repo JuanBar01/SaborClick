21eadb14d141fff0dc2cb2fccbce1962b8b7da41
Juan Barbosa <est.juan.cbarbosa@unimilitar.edu.co>
2025-05-21 03:17:46 -0500

    Colores y rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'restaurantes',
+  loadComponent: () => import('./restaurantes/restaurantes.page').then(m => m.RestaurantesPage)
+  }
 ];
